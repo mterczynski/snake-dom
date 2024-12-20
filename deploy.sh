2ba92f721cb85550a7bc9668fbb5cd4361af55ce
Michał Terczyński <mterczynski1@gmail.com>
2024-12-20 16:58:21 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 # 2. Copy build to ../mterczynski.github.io
 rm -rf ../mterczynski.github.io/snake/
+mkdir ../mterczynski.github.io/snake/
 cp -r ./img ../mterczynski.github.io/snake/
 cp ./index.html ../mterczynski.github.io/snake/
 cp ./main.js ../mterczynski.github.io/snake/
 cd ../mterczynski.github.io
 # 3. Commit in ../mterczynski.github.io
 git add .
-git commit -m "Update snake build: $(git log -1 --pretty=format:"%s")"
+git commit -m "Update snake build"
 git status
 # 4. Push in ../mterczynski.github.io
 git push
